224def4499c1043162c61b37f4f318b00973dda3
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-09-23 14:28:14 +0545

    feat: adbout, gallery

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView,DetailView
 from .forms import BlogForm
-from .models import Blog
+from .models import Blog,Gallery,AboutUs
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name= 'html/index.html'
+    
 
-class AboutView(TemplateView):
+class AboutView(ListView):
+    model=AboutUs
     template_name= 'html/about.html'
+    context_object_name= 'about'
 
 class ContactView(TemplateView):
     template_name= 'html/contact.html'
@@ -26,5 +29,7 @@ class BlogDetailsView(DetailView):
     context_object_name= 'blog'
     
 
-class GalleryView(TemplateView):
+class GalleryView(ListView):
+    model= Gallery
     template_name= 'html/gallery.html'
+    context_object_name= 'gallery'
